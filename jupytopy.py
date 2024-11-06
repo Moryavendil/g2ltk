0e06181ad8a63d168e8548259b32a4a07f15a80b
G2L <gregoire.le-lay@u-paris.fr>
2024-11-06 18:52:07 +0100

    Added mechanism to version ipynb

---
+++ jupytopy.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import os, json
+
+prefix = 'ipynb-'
+ipynbs = [f[:-6] for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.ipynb')]
+ipynbs.sort()
+
+for ipynb in ipynbs:
+    infilename = ipynb + '.ipynb'
+    outfilename = prefix + ipynb + '.py'
+
+    fin = open(infilename, 'r') #input.ipynb
+    j = json.load(fin)
+    fout = open(outfilename, 'w') #output.py
+    fout.write("# -*- coding: utf-8 -*-\n")
+    fout.write("# <nbformat>3.0</nbformat>")
+    if j["nbformat"] >= 4:
+        for i,cell in enumerate(j["cells"]):
+            if cell["cell_type"] == "markdown":
+                fout.write("\n# <markdowncell>\n\n")
+            elif cell["cell_type"] == "code":
+                fout.write("\n# <codecell>\n\n")
+            # of.write("#cell "+str(i)+"\n")
+            for line in cell["source"]:
+                if cell["cell_type"] == "markdown":
+                    fout.write("# ")
+                fout.write(line)
+            fout.write('\n\n')
+    else:
+        for i,cell in enumerate(j["worksheets"][0]["cells"]):
+            fout.write("#cell "+str(i)+"\n")
+            for line in cell["input"]:
+                fout.write(line)
+            fout.write('\n\n')
+
+    fin.close()
+    fout.close()
---
+++ pytojupy.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import os
+from nbformat import v3, v4
+
+prefix = 'ipynb-'
+ipynbs = [f[len(prefix):-3] for f in os.listdir('.') if os.path.isfile(f) and f.endswith('.py') and f.startswith(prefix)]
+ipynbs.sort()
+
+for ipynb in ipynbs:
+    infilename = ipynb + '.ipynb'
+    outfilename = prefix + ipynb + '.py'
+
+    with open(infilename) as fin:
+        text = fin.read()
+
+    text += """
+    # <markdowncell>
+    
+    # If you can read this, reads_py() is no longer broken! 
+    """
+
+    nbook = v3.reads_py(text)
+    nbook = v4.upgrade(nbook)  # Upgrade v3 to v4
+
+    # nbook.metadata.authors = [
+    #     {
+    #         "name": "G2L",
+    #     },
+    # ]
+
+    nbook.metadata.kernelspec = {
+            "display_name": "Python 3",
+            "language": "python",
+            "name": "python3"
+        }
+
+    nbook.metadata.language_info = {
+            "display_name": "Python 3",
+            "language": "python",
+            "name": "python3"
+        }
+
+    nbook.metadata.file_extension = ".py"
+    nbook.metadata.mimetype = "text/x-python"
+    nbook.metadata.name = "python"
+    nbook.metadata.nbconvert_exporter = "python"
+
+    jsonform = v4.writes(nbook) + "\n"
+    with open(outfilename, "w") as fout:
+        fout.write(jsonform)
