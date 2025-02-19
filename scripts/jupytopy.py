21a8808b15597b08cfaf5fdfea82d011cd40a3be
G2L <gregoire.le-lay@u-paris.fr>
2025-02-19 18:45:33 +0100

    Better ipynb saving

--- scripts/jupytopy.py
+++ scripts/jupytopy.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
-import os, json
+import os, sys, json
+
+import argparse
+
+def generate_parser():
+    program_name = 'jupytopy'
+    parser = argparse.ArgumentParser(
+        prog='jupytopy',
+        description=f'{program_name} - jupyter notebooks to python scripts',
+        epilog=f'', add_help=True)
+    # parser.add_argument('-n', metavar='SCRIPTNAME', help='scriptname', type=argparse.FileType('r'))
+    parser.add_argument('-d', metavar='DIRNAME', help='Directory name', type=str)
+    # parser.add_argument('-p', metavar='PXL_DENSITY', help='Pixel density (mm/px)', type=float)
+    # parser.add_argument('-g', metavar='GRAVITY', help='Acceleration of gravity (typically 9.81)', type=float)
+    # parser.add_argument('-d', metavar='DELTARHO', help='Density contrast, in kg/L (typically 1.00 for water/air)', type=float)
+    # parser.add_argument('-o', metavar='OUTPUTFILE', help='Generate graphs [optional:file prefix]', type=str, nargs='?', const='drop', default = None)
+    # parser.add_argument('-v', help='Verbosity (-v: info, -vv: logger.debug, -vvv: trace)', action="count", default=0)
+    return parser
+
+parser = generate_parser()
+
+args = parser.parse_args()
+dirname = args.d
+
 
 maindir = '.'
-scriptsdir = os.path.join(maindir, 'scripts')
+rootscriptsdir = os.path.join(maindir, 'scripts')
+if not os.path.isdir(rootscriptsdir):
+    os.mkdir(rootscriptsdir)
+
+scriptsdir = rootscriptsdir
+if dirname is not None:
+    scriptsdir = os.path.join(rootscriptsdir, dirname)
+
+    if not os.path.isdir(scriptsdir):
+        os.mkdir(scriptsdir)
+
 
 prefix = 'ipynb-'
 ipynbs = [f[:-6] for f in os.listdir(maindir) if os.path.isfile(os.path.join(maindir, f)) and f.endswith('.ipynb')]
--- scripts/pytojupy.py
+++ scripts/pytojupy.py
@@ -1,20 +1,73 @@
 #!/usr/bin/env python
-import os
+import os, sys
 from nbformat import v3, v4
 
+import argparse
+import pathlib
+
+def generate_parser():
+    program_name = 'pytojupy'
+    parser = argparse.ArgumentParser(
+        prog='pytojupy',
+        description=f'{program_name} - python scriptsa to jupyter notebooks',
+        epilog=f'', add_help=True)
+    # parser.add_argument('-n', metavar='SCRIPTNAME', help='scriptname', type=argparse.FileType('r'))
+    parser.add_argument('-n', metavar='SCRIPTPATH', help='script or scipt dir path', type=pathlib.Path)
+    # parser.add_argument('-p', metavar='PXL_DENSITY', help='Pixel density (mm/px)', type=float)
+    # parser.add_argument('-g', metavar='GRAVITY', help='Acceleration of gravity (typically 9.81)', type=float)
+    # parser.add_argument('-d', metavar='DELTARHO', help='Density contrast, in kg/L (typically 1.00 for water/air)', type=float)
+    # parser.add_argument('-o', metavar='OUTPUTFILE', help='Generate graphs [optional:file prefix]', type=str, nargs='?', const='drop', default = None)
+    # parser.add_argument('-v', help='Verbosity (-v: info, -vv: logger.debug, -vvv: trace)', action="count", default=0)
+    return parser
+
+
+parser = generate_parser()
+
+args = parser.parse_args()
+scriptpath = args.n
+
+prefix = 'ipynb-'
 maindir = '.'
 scriptsdir = os.path.join(maindir, 'scripts')
 
-prefix = 'ipynb-'
-ipynbs = [f[len(prefix):-3] for f in os.listdir(scriptsdir) if os.path.isfile(os.path.join(scriptsdir, f)) and f.endswith('.py') and f.startswith(prefix)]
-ipynbs.sort()
+ipynb_names = []
+if scriptpath is None:
+    print('No file selected, converting default scripts')
+    ipynb_names = [f[len(prefix):-3] for f in os.listdir(scriptsdir) if os.path.isfile(os.path.join(scriptsdir, f)) and f.endswith('.py') and f.startswith(prefix)]
+else:
+    if os.path.isdir(scriptpath):
+        # print(f'Directory selected: {scriptpath}')
+        scriptsdir = scriptpath
+        ipynb_names = [f[len(prefix):-3] for f in os.listdir(scriptsdir) if os.path.isfile(os.path.join(scriptsdir, f)) and f.endswith('.py') and f.startswith(prefix)]
+    elif os.path.isfile(scriptpath):
+        scriptsdir = scriptpath.parent
+        scriptname = scriptpath.name
+        if not (scriptname.endswith('.py') & scriptname.startswith(prefix)):
+            print(f'Invalid script name: {scriptname} (path: {scriptpath})')
+            sys.exit(-101)
+        # print(f'File selected: {scriptpath}')
+        ipynb_names = [scriptname[len(prefix):-3]]
+    else:
+        print(f'ERROR - ISWHAT?: {scriptpath}')
+        sys.exit(-100)
 
-for ipynb in ipynbs:
-    infilename = os.path.join(scriptsdir, prefix + ipynb + '.py')
-    outfilename = os.path.join(maindir, ipynb + '.ipynb')
+ipynb_names.sort()
 
-    with open(infilename) as fin:
-        text = fin.read()
+print(f'Scripts directory is: {scriptsdir}')
+print(f'Scripts to convert are: {ipynb_names}')
+
+
+for ipynb_name in ipynb_names:
+    infilename = os.path.join(scriptsdir, prefix + ipynb_name + '.py')
+
+    outfilename = os.path.join(maindir, ipynb_name + '.ipynb')
+
+    try:
+        with open(infilename) as fin:
+            text = fin.read()
+    except:
+        print(f'ERROR - CANNOT OPEN?: {infilename}')
+        sys.exit(-201)
 
     text += """
     # <markdowncell>
@@ -49,5 +102,10 @@ for ipynb in ipynbs:
     nbook.metadata.nbconvert_exporter = "python"
 
     jsonform = v4.writes(nbook) + "\n"
-    with open(outfilename, "w") as fout:
-        fout.write(jsonform)
+
+    try:
+        with open(outfilename, "w") as fout:
+            fout.write(jsonform)
+    except:
+        print(f'ERROR - CANNOT OPEN?: {outfilename}')
+        sys.exit(-202)
