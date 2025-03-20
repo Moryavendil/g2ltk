3a48e4691475e0b1dcdbc8694fb4aaa0404fad62
G2L <gregoire.le-lay@u-paris.fr>
2025-03-20 19:26:59 +0100

    Small useful default settings saved

--- g2ltk/utility/plotting.py
+++ g2ltk/utility/plotting.py
@@ -1,4 +1,15 @@
 
+
+# default settings
+errorbar_kw_default = {'capsize': 3, 'ls': '', 'marker':'o', 'mfc': 'w'}
+fill_between_kw_default = {'alpha':.1, 'lw':0.0}
+
+
+# rivulet colors
+color_q = '#9c1ab2'
+color_w = '#3d5da9'
+color_z = '#ff1a1a' # (dark version : #9c1ab2 | light version : #c320df
+
 def force_aspect_ratio(ax, aspect=1):
     im = ax.get_images()
     extent =  im[0].get_extent()
