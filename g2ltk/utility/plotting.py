f137bd46cc4cc826ff4ed9154226afe0a0156b76
G2L <gregoire.le-lay@u-paris.fr>
2025-03-21 10:54:19 +0100

    Added flow rate condition colors

--- g2ltk/utility/plotting.py
+++ g2ltk/utility/plotting.py
@@ -6,9 +6,13 @@ fill_between_kw_default = {'alpha':.1, 'lw':0.0}
 
 
 # rivulet colors
-color_q = '#9c1ab2'
 color_w = '#3d5da9'
-color_z = '#ff1a1a' # (dark version : #9c1ab2 | light version : #c320df
+color_z = '#ff1a1a'
+color_q = '#9c1ab2' # (dark version : #9c1ab2 | light version : #c320df
+
+# condition colors
+smallQ_color = '#008C00'
+bigQ_color = '#C29A49'
 
 def force_aspect_ratio(ax, aspect=1):
     im = ax.get_images()
