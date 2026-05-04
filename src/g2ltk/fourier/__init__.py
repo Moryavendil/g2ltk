df1050eb68623c3afb8de1180d2f466b348af4fc
G2L <gregoire.le-lay@u-paris.fr>
2026-05-04 09:03:54 +0200

    Added zpf as alias for zero_pad_factor

--- src/g2ltk/fourier/__init__.py
+++ src/g2ltk/fourier/__init__.py
@@ -48,34 +48,56 @@ def log_amplitude_cbticks(maximum_amplitude: float, range_db: Union[int, float])
 
 ### FFT AND PSD COMPUTATIONS
 
+from functools import wraps
+
+def alias_argument(new_name, old_name):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            if old_name in kwargs:
+                if new_name in kwargs:
+                    raise TypeError(f"Received both '{new_name}' and '{old_name}'")
+                kwargs[new_name] = kwargs.pop(old_name)
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
 from .FFT1D import *
 
 from .FFT2D import *
 
 
-def dual(x: floatarray1D, y: floatarray1D, zero_pad_factor=None):
+# todo: replace zero_pad by nfft
+# todo accept 'auto' as nfft, which selects the nearest superior 5-smooth number
+
+@alias_argument('zpf', 'zero_pad_factor')
+def dual(x: floatarray1D, y: floatarray1D, zpf=None):
     if y is None:
-        return dual1d(x, zero_pad_factor=zero_pad_factor)
-    return dual2d(x, y=y, zero_pad_factor=zero_pad_factor)
+        return dual1d(x, zero_pad_factor=zpf)
+    return dual2d(x, y=y, zero_pad_factor=zpf)
 
 
-def rdual(x: floatarray1D, zero_pad_factor=None):
-    return rdual1d(x, zero_pad_factor=zero_pad_factor)
+@alias_argument('zpf', 'zero_pad_factor')
+
+def rdual(x: floatarray1D, zpf=None):
+    return rdual1d(x, zero_pad_factor=zpf)
 
 
+@alias_argument('zpf', 'zero_pad_factor')
 def ft(sig, x: Optional[np.ndarray] = None, y: Optional[np.ndarray] = None,
        window: str = default_window, winstyle=None, remove_mean: bool = True,
-       zero_pad_factor=None, shift=None):
+       zpf=None, shift=None):
     if np.ndim(sig) == 2:
         return ft2d(sig, x=x, y=y, window=window, winstyle=winstyle, remove_mean=remove_mean,
-                    zero_pad_factor=zero_pad_factor, shift=shift)
+                    zero_pad_factor=zpf, shift=shift)
     if np.ndim(sig) == 1:
         if y is not None:
             raise RuntimeError('y given but sig is 1-D?')
         if winstyle is not None:
             raise RuntimeError('winstyle given but sig is 1-D?')
         return ft1d(sig, x=x, window=window, remove_mean=remove_mean,
-                    zero_pad_factor=zero_pad_factor, shift=shift)
+                    zero_pad_factor=zpf, shift=shift)
     raise RuntimeError(f'?? Called ft but sig is {np.ndim(sig)}-dimensional')
 
 
@@ -89,23 +111,25 @@ def ift(sig_hat: complexarray1D, xdual: Optional[np.ndarray] = None, ydual: Opti
     raise RuntimeError(f'?? Called ft but sig_hat is {np.ndim(sig_hat)}-dimensional')
 
 
+@alias_argument('zpf', 'zero_pad_factor')
 def rft(sig, x: Optional[np.ndarray] = None,
         window: str = default_window, remove_mean: bool = True,
-        zero_pad_factor=None, shift=None):
+        zpf=None, shift=None):
     if np.ndim(sig) == 1:
         return rft1d(sig, x=x, window=window, remove_mean=remove_mean,
-                     zero_pad_factor=zero_pad_factor, shift=shift)
+                     zero_pad_factor=zpf, shift=shift)
     raise RuntimeError(f'?? Called rft but sig is {np.ndim(sig)}-dimensional')
 
 
+@alias_argument('zpf', 'zero_pad_factor')
 def psd(sig, x: Optional[np.ndarray] = None, y: Optional[np.ndarray] = None,
         window: str = default_window, winstyle=None, remove_mean: bool = True,
-        zero_pad_factor=None, welch_factor=None):
+        zpf=None, welch_factor=None):
     if np.ndim(sig) == 2:
         if welch_factor is not None:
             raise NotImplementedError('welch 2d not implemented yet')
         return psd2d(sig, x=x, y=y, window=window, winstyle=winstyle, remove_mean=remove_mean,
-                     zero_pad_factor=zero_pad_factor)
+                     zero_pad_factor=zpf)
     if np.ndim(sig) == 1:
         if y is not None:
             raise RuntimeError('y given but sig is 1-D?')
@@ -113,7 +137,7 @@ def psd(sig, x: Optional[np.ndarray] = None, y: Optional[np.ndarray] = None,
             raise RuntimeError('winstyle given but sig is 1-D?')
         if welch_factor is not None:
             return welch1d(sig, x=x, window=window, remove_mean=remove_mean,
-                           zero_pad_factor=zero_pad_factor, welch_factor=welch_factor)
+                           zero_pad_factor=zpf, welch_factor=welch_factor)
         return psd1d(sig, x=x, window=window, remove_mean=remove_mean,
-                     zero_pad_factor=zero_pad_factor)
+                     zero_pad_factor=zpf)
     raise RuntimeError('y given but sig is 1-D?')
